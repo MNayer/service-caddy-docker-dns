2f8995d9c23ef36ab8c216affdda1d8c4d579abd
Felix Weissberg <anon@anaon.com>
2026-01-16 17:41:50 +0100

    Update setup and update routine

--- setup.sh
+++ setup.sh
@@ -12,6 +12,20 @@ if [ $(docker network ls | grep ${network_name} | wc -l) -eq 0 ]; then
 	docker network create ${network_name}
 fi
 
-sudo apt-get install -y jq
+echo "[+] Install requirements."
+sudo apt-get install -y jq curl
+
+echo "[+] Create Dockerfile."
+./update_docker.sh
+
+echo "[+] Build image."
 ./build_container.sh
+
+echo "[*] Setup systemd services"
+sudo cp caddy-docker-dns-update.service /etc/systemd/system/
+sudo cp caddy-docker-dns-update.timer /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable --now caddy-docker-dns-update.timer
+
+echo "[+] Start service."
 docker compose up -d
--- update.sh
+++ update.sh
@@ -2,28 +2,13 @@
 
 set -e
 
-echo "[+] Get most recent tags."
-digest_latest=$(curl -s "https://registry.hub.docker.com/v2/repositories/library/caddy/tags?page_size=100" \
-	| jq -r '.results[] | "\(.name) \(.digest)"' \
-	| grep 'latest' \
-	| cut -d' ' -f2 \
-	| cut -d':' -f 2
-)
+echo "[+] Update Dockerfile."
+./update_docker.sh
 
-echo "[+] Find the version that's equivalent to the latest tag."
-version_latest=$(curl -s "https://registry.hub.docker.com/v2/repositories/library/caddy/tags?page_size=100" \
-	| jq -r '.results[] | "\(.name) \(.digest)"' \
-	| grep "${digest_latest}" \
-	| grep -v "latest" \
-	| sort -V \
-	| tail -n1 \
-	| cut -d' ' -f1
-)
+echo "[+] Build new image."
+./build.sh
 
-echo "[+] Found latest versoin: '${version_latest}'. Create Dockerfile from template."
-cat Dockerfile.template | sed "s/CADDYVERSION/${version_latest}/" > Dockerfile
-
-echo "[+] Start update."
+echo "[+] Restart service."
 docker compose down
 docker compose up -d
 
